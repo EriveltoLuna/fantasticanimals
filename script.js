3af6eaffa6679acdf721134e04776b1e31dba57e
Velto <eriveltoferreiralunafilho@gmail.com>
2024-09-26 09:24:55 -0300

    bugfixed classList on scroll to top btn

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+//scroll to top button
 let btn = document.querySelector(".btn");
 
 window.onscroll = () => {
   if (document.body.scrollTop < 20) {
-    btn.classList.remove = "active";
+    btn.classList.remove("active");
   } else {
-    btn.classList.add = "active";
+    btn.classList.add("active");
   }
 };
 
