d172cbed81490abad80103d125be505c5dd45792
Velto <eriveltoferreiralunafilho@gmail.com>
2024-09-26 00:02:58 -0300

    started javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+let btn = document.querySelector(".btn");
+
+window.onscroll = () => {
+  if (document.body.scrollTop < 20) {
+    btn.classList.remove = "active";
+  } else {
+    btn.classList.add = "active";
+  }
+};
+
+btn.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+});
