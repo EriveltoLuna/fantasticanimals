b78e4b064dbde3f5b32d0197df4b6fa51bc7daf2
Velto <eriveltoferreiralunafilho@gmail.com>
2024-09-26 20:19:33 -0300

    some exercises on js front

--- script.js
+++ script.js
@@ -12,3 +12,37 @@ window.onscroll = () => {
 btn.addEventListener("click", () => {
   document.body.scrollTop = 0;
 });
+
+// Verifique a distância da primeira imagem
+// em relação ao topo da página
+const imgTop = document.querySelector("img");
+console.log(imgTop.offsetTop);
+
+// Retorne a soma da largura de todas as imagens
+
+function sumWidth() {
+  let sum = 0;
+  const imgsWidth = document.querySelectorAll("img");
+  imgsWidth.forEach((imgWidth) => {
+    sum += imgWidth.offsetWidth;
+  });
+  console.log(sum);
+}
+
+window.onload = sumWidth();
+
+// Verifique se os links da página possuem
+// o mínimo recomendado para telas utilizadas
+// com o dedo. (48px/48px de acordo com o google)
+
+// e se o browser for menor que 720px,
+// adicione a classe menu-mobile ao menu
+
+const links = document.querySelectorAll(".menu a");
+
+links.forEach((link) => {
+  let linkData = link.getBoundingClientRect();
+  if ((linkData.height || linkData.width < 48) && window.innerHeight < 720) {
+    link.classList.add("menu-mobile");
+  }
+});
